2c9b0d3cd25af650edb7a42d7c39e6cc44edc14b
Bartosz Zawada <bataax@gmail.com>
2020-11-08 21:59:55 +0100

    FIX DB

--- connect.php
+++ connect.php
@@ -1,8 +1,14 @@
 <?php
 
-	$host = "localhost";
-	$db_user = "root";
-	$db_password = "";
-	$db_name = "projekt_1_ZAI";
+//	$host = "localhost";
+	//$db_user = "root";
+	//$db_password = "";
+	//$db_name = "projekt_1_ZAI";
+
+	$host = "ryfqldzbliwmq6g5.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+	$db_user = "ay9zf1wot38v1bpg";
+	$db_password = "oy1ckawwfa4pxh4k";
+	$db_name = "fdo4chiae77lt0q3";
+
 	
 ?>
